668ded50c1d6a5707f781c1763f463fd592fa503
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-05-24 19:47:50 -0400

    Update creating a blank window.py

--- creating a blank window.py
+++ creating a blank window.py
@@ -3,7 +3,19 @@ import pygame
 #Initiate pygame
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((800,800))
+gameDisplay = pygame.display.set_mode((500,500))
 pygame.display.set_caption("A bit Racey")
 
 clock = pygame.time.Clock()
+
+isRunning = True
+
+#Load an image.
+carImage = pygame.image.load("car.png")
+
+#Main program
+while isRunning:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRunning = False
